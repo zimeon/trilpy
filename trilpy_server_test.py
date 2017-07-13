1104576a287219743858542b93671e5bfce86a18
Simeon Warner <simeon.warner@cornell.edu>
2017-07-13 00:53:18 -0400

    Flag to run against specified instance or start server

--- trilpy_server_test.py
+++ trilpy_server_test.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python
 """Test trilpy by running on localhost."""
+import argparse
 import unittest
 from urllib.parse import urljoin
 from subprocess import Popen
 import re
 import requests
+import sys
 import time
+import uuid
 
 
 class TestAll(unittest.TestCase):
     """TestAll class to run tests."""
 
     port = 9999
-    baseuri = 'http://localhost:' + str(port) + '/'
-    rooturi = baseuri
+    rooturi = 'http://localhost:' + str(port) + '/'
+    start_trilpy = True
+    new_for_each_test = False
 
-    def setUp(self):
+    @classmethod
+    def _start_trilpy(cls):
         """Start trilpy."""
-        self.proc = Popen(['/usr/bin/env', 'python', './trilpy.py',
-                           '-v', '-p', str(self.port)])
-        print("Started trilpy (pid=%d)" % (self.proc.pid))
+        cls.proc = Popen(['/usr/bin/env', 'python', './trilpy.py',
+                          '-v', '-p', str(cls.port)])
+        print("Started trilpy (pid=%d)" % (cls.proc.pid))
         time.sleep(2)
 
-    def tearDown(self):
+    @classmethod
+    def _stop_trilpy(cls):
         """Kill trilpy."""
-        self.proc.kill()
-        outs, errs = self.proc.communicate()
+        cls.proc.kill()
+        outs, errs = cls.proc.communicate()
         print("Killed trilpy (%s, %s)" % (outs, errs))
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup for class."""
+        if (cls.start_trilpy and not cls.new_for_each_test):
+            cls._start_trilpy()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Teardown for class."""
+        if (cls.start_trilpy and not cls.new_for_each_test):
+            cls._stop_trilpy()
+
+    def setUp(self):
+        """Setup for each test."""
+        if (self.start_trilpy and self.new_for_each_test):
+            self._start_trilpy()
+
+    def tearDown(self):
+        """Teardown for each test."""
+        if (self.start_trilpy and self.new_for_each_test):
+            self._stop_trilpy()
 
     def find_links(self, link_header, rel):
         """Find list of link values with given rel."""
@@ -51,7 +79,7 @@ class TestAll(unittest.TestCase):
 
     def test01_unknown_paths(self):
         """Expect 404 for bad path."""
-        url = urljoin(self.baseuri, 'does_not_exist')
+        url = urljoin(self.rooturi, 'does_not_exist')
         r = requests.get(url)
         self.assertEqual(r.status_code, 404)
         r = requests.head(url)
@@ -63,20 +91,20 @@ class TestAll(unittest.TestCase):
 
     def test02_b(self):
         """Another test."""
-        r = requests.get(urljoin(self.baseuri, '/'))
+        r = requests.get(urljoin(self.rooturi, '/'))
         self.assertEqual(r.status_code, 200)
 
-    def test03_delete_root_ldpc(self):
-        """Delete the LDPC at /."""
-        url = urljoin(self.baseuri, '/')
-        r = requests.delete(url)
-        self.assertEqual(r.status_code, 200)
-        r = requests.get(url)
-        self.assertEqual(r.status_code, 410)
+#    def test03_delete_root_ldpc(self):
+#        """Delete the LDPC at /."""
+#        url = urljoin(self.rooturi, '/')
+#        r = requests.delete(url)
+#        self.assertEqual(r.status_code, 200)
+#        r = requests.get(url)
+#        self.assertEqual(r.status_code, 410)
 
     def test_ldp_4_2_4_5(self):
         """If-Match on ETag for PUT to replace."""
-        url = urljoin(self.baseuri, '/test1')
+        url = urljoin(self.rooturi, '/' + str(uuid.uuid1()))
         # PUT object
         r = requests.put(url,
                          headers={'Content-Type': 'text/turtle'},
@@ -102,6 +130,8 @@ class TestAll(unittest.TestCase):
                                   'Content-Type': 'text/turtle'},
                          data='<http://ex.org/a> <http://ex.org/b> "3".')
         self.assertEqual(r.status_code, 412)
+        # Cleanup
+        r = requests.delete(url)
 
     def test_fedora_5_1(self):
         """Check ACLs are LDP RDF Sources."""
@@ -178,4 +208,22 @@ class TestAll(unittest.TestCase):
 
 # If run from command line, do tests
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--rooturi', '-r', action='store', default='',
+                        help="Use LDP at given rooturi rather than running trilpy")
+    parser.add_argument('--fresh', action='store_true',
+                        help="Start trilpy fresh for each test (slow)")
+    parser.add_argument('--port', '-p', type=int, default=9999,
+                        help="Start trilp on port (default %default)")
+    parser.add_argument('--VeryVerbose', '-V', action='store_true',
+                        help="be verbose.")
+    (opts, args) = parser.parse_known_args()
+    TestAll.port = opts.port
+    if (opts.rooturi):
+        TestAll.start_trilpy = False
+        TestAll.rooturi = opts.rooturi
+    else:
+        TestAll.new_for_each_test = opts.fresh
+    # Remaining args go to unittest
+    unittest.main(verbosity=(2 if opts.VeryVerbose else 1),
+                  argv=sys.argv[:1] + args)
