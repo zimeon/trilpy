50bd92e63ed33b6b71bc7e0cf6d20595b9a36032
Simeon Warner <simeon.warner@cornell.edu>
2017-09-06 23:04:54 -0400

    Get with the argparse program, replace optparse

--- trilpy.py
+++ trilpy.py
@@ -3,57 +3,59 @@
 
 import sys
 import logging
-import optparse
+import argparse
 from trilpy import Store, LDPC, ACLR, LDP, run
+
 
 def main():
     """Command line handler."""
-    parser = optparse.OptionParser()
-    parser.add_option('--port', '-p', type='int', default=9999,
-                      help="port to run on (default %default)")
-    parser.add_option('--root-container', '-r', default='/',
-                      help="define root container path (default %default)")
-    parser.add_option('--container-type', '-c', default='basic',
-                      help="root container type (default %default)")
-    parser.add_option('--no-put', action='store_true',
-                      help="do not support PUT method")
-    parser.add_option('--no-delete', action='store_true',
-                      help="do not support DELETE method")
-    parser.add_option('--no-acl', action='store_true',
-                      help="do not support Web Access Control ACLs")
-    parser.add_option('--root-acl', default='/.acl',
-                      help="define root ACL path (default %default)")
-    parser.add_option('--default-acl', default='/default.acl',
-                      help="define default ACL path (default %default)")
-    parser.add_option('--verbose', '-v', action='store_true',
-                      help="be verbose.")
-    (opts, args) = parser.parse_args()
-    if (opts.container_type == 'basic'):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--port', '-p', type=int, default=9999,
+                        help="port to run on")
+    parser.add_argument('--root-container', '-r', default='/',
+                        help="define root container path")
+    parser.add_argument('--container-type', '-c', default='basic',
+                        help="root container type")
+    parser.add_argument('--no-put', action='store_true',
+                        help="do not support PUT method")
+    parser.add_argument('--no-delete', action='store_true',
+                        help="do not support DELETE method")
+    parser.add_argument('--no-acl', action='store_true',
+                        help="do not support Web Access Control ACLs")
+    parser.add_argument('--root-acl', default='/.acl',
+                        help="define root ACL path")
+    parser.add_argument('--default-acl', default='/default.acl',
+                        help="define default ACL path")
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help="be verbose.")
+    args = parser.parse_args()
+    if (args.container_type == 'basic'):
         container_type = LDP.BasicContainer
-    elif (opts.container_type == 'direct'):
+    elif (args.container_type == 'direct'):
         container_type = LDP.DirectContainer
-    elif (opts.container_type == 'indirect'):
+    elif (args.container_type == 'indirect'):
         container_type = LDP.IndirectContainer
     else:
         parser.error("Unrecognized container type '%s'" %
-                     (opts.container_type))
-    logging.basicConfig(level=logging.DEBUG if opts.verbose else logging.INFO)
-    base_uri = 'http://localhost:%d' % (opts.port)  # FIXME
+                     (args.container_type))
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    base_uri = 'http://localhost:%d' % (args.port)  # FIXME
     store = Store(base_uri)
     container = LDPC(container_type=container_type)
-    store.add(container, opts.root_container)
-    if (not opts.no_acl):
-        acl = ACLR(acl_for=opts.root_container)
+    store.add(container, args.root_container)
+    if (not args.no_acl):
+        acl = ACLR(acl_for=args.root_container)
         acl.add_public_read(inherit=True)
-        acl_uri = store.add(acl, opts.root_acl)
+        acl_uri = store.add(acl, args.root_acl)
         container.acl = acl_uri
-        acl_default = ACLR(acl_for=opts.default_acl)  # for self hack
+        acl_default = ACLR(acl_for=args.default_acl)  # for self hack
         acl_default.add_public_read(inherit=True)
-        acl.acl_default = store.add(acl, opts.default_acl)
-    run(opts.port, store,
-        support_put=(not opts.no_put),
-        support_delete=(not opts.no_delete),
-        support_acl=(not opts.no_acl))
+        acl.acl_default = store.add(acl, args.default_acl)
+    run(args.port, store,
+        support_put=(not args.no_put),
+        support_delete=(not args.no_delete),
+        support_acl=(not args.no_acl))
 
 if __name__ == "__main__":
     main()
