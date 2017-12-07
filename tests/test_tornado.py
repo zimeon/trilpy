30815f1c0c676095f916e38bb07f0fd16ef1ee42
Simeon Warner <simeon.warner@cornell.edu>
2017-12-07 18:32:12 -0500

    Make tests match new structures

--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -18,47 +18,43 @@ class TestAll(unittest.TestCase):
         h = LDPHandler(tornado.web.Application(), Mock())
         h.request.headers = Mock()
         h.request.headers.get_list = Mock(return_value=[])
-        self.assertEqual(h.request_types, set())
-        h._types = None
-        h.request.headers.get_list = Mock(return_value=['link_header_1', 'link_header_2'])
-        with self.assertRaises(HTTPError):
-            h.request_types
-        h._types = None
+        self.assertEqual(h.request_types, [])
+        h._request_links = None
         h.request.headers.get_list = Mock(return_value=['a'])
-        self.assertEqual(h.request_types, set())
-        h._types = None
+        self.assertEqual(h.request_types, [])
+        h._request_links = None
         h.request.headers.get_list = Mock(return_value=['<bb>; rel="type"'])
-        self.assertEqual(h.request_types, set(['bb']))
-        h._types = None
+        self.assertEqual(h.request_types, ['bb'])
+        h._request_links = None
         h.request.headers.get_list = Mock(return_value=['<cc>; rel="type", <dd>; rel="type", <ee>; rel="other", <ff>; rel="type"'])
-        self.assertEqual(h.request_types, set(['cc', 'dd', 'ff']))
+        self.assertEqual(h.request_types, ['cc', 'dd', 'ff'])
 
     def test21_request_ldp_type(self):
-        """Test LDPHandler.request_ldp_type."""
+        """Test LDPHandler.request_ldp_type() method."""
         h = LDPHandler(tornado.web.Application(), Mock())
-        h._types = set(['bb'])
+        h._request_links = {'type': ['bb']}
         self.assertEqual(h.request_ldp_type(), None)
-        h._types = set(['http://www.w3.org/ns/ldp#NonRDFSource'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#NonRDFSource']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#NonRDFSource')
-        h._types = set(['http://www.w3.org/ns/ldp#NonRDFSource', 'cc'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#NonRDFSource', 'cc']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#NonRDFSource')
-        h._types = set(['dd', 'http://www.w3.org/ns/ldp#NonRDFSource'])
+        h._request_links = {'type': ['dd', 'http://www.w3.org/ns/ldp#NonRDFSource']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#NonRDFSource')
-        h._types = set(['http://www.w3.org/ns/ldp#RDFSource'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#RDFSource']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#RDFSource')
-        h._types = set(['http://www.w3.org/ns/ldp#RDFSource',
-                        'http://www.w3.org/ns/ldp#NonRDFSource'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#RDFSource',
+                                     'http://www.w3.org/ns/ldp#NonRDFSource']}
         self.assertRaises(HTTPError, h.request_ldp_type)
-        h._types = set(['http://www.w3.org/ns/ldp#RDFSource',
-                        'http://www.w3.org/ns/ldp#Container'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#RDFSource',
+                                     'http://www.w3.org/ns/ldp#Container']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#Container')
-        h._types = set(['http://www.w3.org/ns/ldp#BasicContainer',
-                        'http://www.w3.org/ns/ldp#Container'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#BasicContainer',
+                                     'http://www.w3.org/ns/ldp#Container']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#BasicContainer')
-        h._types = set(['http://www.w3.org/ns/ldp#Container',
-                        'http://www.w3.org/ns/ldp#DirectContainer'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#Container',
+                                     'http://www.w3.org/ns/ldp#DirectContainer']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#DirectContainer')
         # FIXME - Following should probably give HTTPError
-        h._types = set(['http://www.w3.org/ns/ldp#BasicContainer',
-                        'http://www.w3.org/ns/ldp#DirectContainer'])
+        h._request_links = {'type': ['http://www.w3.org/ns/ldp#BasicContainer',
+                                     'http://www.w3.org/ns/ldp#DirectContainer']}
         self.assertEqual(h.request_ldp_type(), 'http://www.w3.org/ns/ldp#DirectContainer')
